8fe24a13a508a471e7e0a6efe060a90fcfc536da
tangibleideas <184009480+tangibleideas@users.noreply.github.com>
2025-09-12 12:34:59 +0200

    initial commit: spring boot starter

---
+++ src/test/java/com/tangibleideas/todolistservice/TodoListServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tangibleideas.todolistservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
