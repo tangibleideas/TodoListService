ec8955c2fe040050b8c8952569cdba1cbdec1b10
tangibleideas <184009480+tangibleideas@users.noreply.github.com>
2025-09-12 12:52:26 +0200

    Add checkstyle and javaformat plugins

--- src/main/java/com/tangibleideas/todolistservice/TodoListServiceApplication.java
+++ src/main/java/com/tangibleideas/todolistservice/TodoListServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TodoListServiceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(TodoListServiceApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListServiceApplication.class, args);
+	}
 
 }
